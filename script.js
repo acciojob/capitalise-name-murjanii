9b7932302c2f1dacb2467250a9d1fdb1d3861371
murjanii <153760405+murjanii@users.noreply.github.com>
2025-05-12 23:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function toUpperCaseOnBlur() {
+  var inputField = document.getElementById("fname");
+  inputField.value = inputField.value.toUpperCase();
+}
+
+window.onload = function() {
+  var inputField = document.getElementById("fname");
+  inputField.addEventListener("blur", toUpperCaseOnBlur);
+};
